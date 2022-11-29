1f2d66f795f78c7210815ea555901dacace566da
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2022-11-29 17:36:01 -0400

    fix comments.

--- src/services/fileManagement.js
+++ src/services/fileManagement.js
@@ -1,5 +1,5 @@
 /*************************************************************************************
- * Product: ADempiere gRPC File Management Client                                *
+ * Product: ADempiere gRPC File Management Client                                    *
  * Copyright (C) 2012-2022 E.R.P. Consultores y Asociados, C.A.                      *
  * Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com                      *
  * This program is free software: you can redistribute it and/or modify              *
