11e412243381dadfa005e79db5f472c4543c0d02
Edwin Betancourt <EdwinBetanc0urt@outlook.com>
2023-01-24 15:50:01 -0400

    feat: Add dashboard notice. (#86)

--- src/utils/convertEnums.js
+++ src/utils/convertEnums.js
@@ -1,5 +1,5 @@
 /*************************************************************************************
- * Product: ADempiere gRPC Business Data Client Convert Utils                        *
+ * Product: ADempiere gRPC Enum Convert Util                                         *
  * Copyright (C) 2012-2020 E.R.P. Consultores y Asociados, C.A.                      *
  * Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com                      *
  * This program is free software: you can redistribute it and/or modify              *
@@ -8,30 +8,51 @@
  * (at your option) any later version.                                               *
  * This program is distributed in the hope that it will be useful,                   *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of                    *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                     *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                      *
  * GNU General Public License for more details.                                      *
  * You should have received a copy of the GNU General Public License                 *
  * along with this program. If not, see <https://www.gnu.org/licenses/>.             *
  ************************************************************************************/
 
-const convertEnums = {
-
-  /**
-   * Get all values type or get key value type from value
-   * @param {object} list
-   * @param {string} key
-   * @param {number} value
-   * @returns {number|string|object}
-   */
-  getValueOrKey({ list, key, value }) {
-    if (key !== undefined) {
-      return list[key];
-    } else if (value !== undefined) {
-      return Object.keys(list).find(keyItem => list[keyItem] === value);
-    }
-    // return all values
-    return list;
-  }
+/**
+ * Get all values type or get key value type from value
+ * @deprecated
+ * @param {object} list
+ * @param {string} key
+ * @param {number} value
+ * @returns {number|string|object}
+ */
+function getValueOrKey({ list, key, value }) {
+  // return all values
+  return getValueOrKeyEnum({
+    list,
+    key,
+    value
+  });
 }
 
-module.exports = convertEnums;
+/**
+ * Get all values type or get key value type from value
+ * @param {object} list
+ * @param {string} key
+ * @param {number} value
+ * @returns {number|string|object}
+ */
+function getValueOrKeyEnum({ list, key, value }) {
+  if (isEmptyValue(list)) {
+    return undefined;
+  }
+
+  if (key !== undefined) {
+    return list[key];
+  } else if (value !== undefined) {
+    return Object.keys(list).find(keyItem => list[keyItem] === value);
+  }
+  // return all values
+  return list;
+}
+
+module.exports = {
+  getValueOrKey,
+  getValueOrKeyEnum
+};
