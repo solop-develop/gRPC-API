03b06a92c0319e7ee2646965e88b8f099b50bc97
EdwinBetanc0urt <EdwinBetanc0urt@outlook.com>
2023-06-05 16:28:28 -0400

    fix: Callout empty context values.

--- src/utils/baseDataTypeFromGRPC.js
+++ src/utils/baseDataTypeFromGRPC.js
@@ -1,6 +1,6 @@
 /*************************************************************************************
  * Product: ADempiere gRPC Base Data Type Client Convert Utils                       *
- * Copyright (C) 2012-2023 E.R.P. Consultores y Asociados, C.A.                      *
+ * Copyright (C) 2018-2023 E.R.P. Consultores y Asociados, C.A.                      *
  * Contributor(s): Edwin Betancourt EdwinBetanc0urt@outlook.com                      *
  * This program is free software: you can redistribute it and/or modify              *
  * it under the terms of the GNU General Public License as published by              *
@@ -8,7 +8,7 @@
  * (at your option) any later version.                                               *
  * This program is distributed in the hope that it will be useful,                   *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of                    *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                     *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                      *
  * GNU General Public License for more details.                                      *
  * You should have received a copy of the GNU General Public License                 *
  * along with this program. If not, see <https://www.gnu.org/licenses/>.             *
@@ -154,7 +154,7 @@ function getValueFromGRPC(valueToConvert) {
       returnValue = undefined;
       break;
   }
-  return returnValue; 
+  return returnValue;
 }
 
 /**
